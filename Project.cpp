9fceef7e5ae3f73c17e98566ba7c237808e4a1ea
viettungvuong <viettungvuong@gmail.com>
2022-10-30 09:10:30 +0700

    Thêm cái xe vào thành công

--- Project.cpp
+++ Project.cpp
@@ -1,23 +1,30 @@
 ﻿
 #include "graphics.h"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    loadTexture();
-    while (window.isOpen())
-    {
+void main(int argc, char** argv[]) {
+    sf::RenderWindow window(sf::VideoMode(640, 480),
+        "Bouncing car.");
+
+    sf::Texture carTexture;
+    carTexture.loadFromFile("car.png");
+    sf::Sprite car(carTexture);
+    sf::Vector2u size = carTexture.getSize();
+    car.setOrigin(size.x / 2, size.y / 2);
+    sf::Vector2f increment(0.4f, 0.4f);
+    car.scale(sf::Vector2f(0.25f, 0.25f));
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
 
-        window.clear();
-        window.draw(sprite);
+        car.setPosition(sf::Vector2f(250.f, 100.f));
+
+        window.clear(sf::Color(16, 16, 16, 255)); // Dark gray.
+        window.draw(car); // Drawing our sprite.
         window.display();
     }
-
-    return 0;
 }
--- graphics.cpp
+++ graphics.cpp
@@ -3,8 +3,8 @@
 sf::Texture texture;
 sf::Sprite sprite;
 void loadTexture() {
-	if (!texture.loadFromFile("car.png"))
-		std::cout<<"Fail to load";
+	texture.loadFromFile("car.png");
+
 	texture.setSmooth(true); //lam diu texture
 	sprite.setTexture(texture); //tao sprite
 	//sprite.setColor(sf::Color(0, 255, 0));
