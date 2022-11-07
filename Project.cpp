9b039b25b1b2041352cb9e840c958f18927792bd
viettungvuong <viettungvuong@gmail.com>
2022-11-07 17:31:18 +0700

    Chỉnh cách tạo xe

--- Project.cpp
+++ Project.cpp
@@ -14,19 +14,14 @@ void main(int argc, char** argv[]) {
 	sf::Time elapsed;
 
 	//tao xe
-	//de lam object pooling sau
+	//de lam object pooling sau	
+	//de coi con cach nao khong
 	CCAR newCar;
-	CCAR newCar2(-500);
-	CCAR newCar3(-1000);
-	CCAR newCar4(-1500);
-	CCAR newCar5(-2000);
-	CCAR newCar6(-2500);
-	generatedCars.push_back(newCar);
-	generatedCars.push_back(newCar2);
-	generatedCars.push_back(newCar3);
-	generatedCars.push_back(newCar4);
-	generatedCars.push_back(newCar5);
-	generatedCars.push_back(newCar6);
+	for (int i = 0; i < 6; i++) {
+		newCar = CCAR(-500 * i);
+		generatedCars.push_back(newCar);
+	}
+
 	std::cout << generatedCars.size();
 
 	const sf::Time update_ms = sf::seconds(1.f / 30.f);
