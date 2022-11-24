3258bc4c1ecd95ad52d1dd540e4c9434f76d6474
viettungvuong <viettungvuong@gmail.com>
2022-11-24 15:40:28 +0700

    Thêm code menu

--- menu.cpp
+++ menu.cpp
@@ -1,4 +1,5 @@
 #include "menu.h"
+#include <stdio.h>
 int menu, currentIndex;
 void eventHandler()
 {
@@ -30,7 +31,7 @@ void keyHandler(KEY_EVENT_RECORD key) {
 		switch (key.wVirtualKeyCode) {
 		case KEY_UP:
 			clrscr();
-
+			//cho nay la co menu
 		}
 	}
 }
@@ -65,4 +66,92 @@ SHORT wherey()
 	console::output = GetStdHandle(STD_OUTPUT_HANDLE);
 	GetConsoleScreenBufferInfo(console::output, &coninfo);
 	return coninfo.dwCursorPosition.Y;
+}
+
+void datMauNen(WORD color)
+{
+	console::output = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(console::output, &screen_buffer_info);
+
+	WORD wAttributes = screen_buffer_info.wAttributes;
+	color &= 0x000f;
+	color <<= 4; // Dich trai 3 bit de phu hop voi mau nen
+	wAttributes &= 0xff0f; // Cai 0 cho 1 bit chu nhay va 3 bit mau nen
+	wAttributes |= color;
+
+	SetConsoleTextAttribute(console::output, wAttributes);
+}
+
+void datMau(WORD color)
+{
+	console::output = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(console::output, &screen_buffer_info);
+
+	WORD wAttributes = screen_buffer_info.wAttributes;
+	color &= 0x000f;
+	wAttributes &= 0xfff0;
+	wAttributes |= color;
+
+	SetConsoleTextAttribute(console::output, wAttributes);
+}
+
+WORD textattr()
+{
+	CONSOLE_SCREEN_BUFFER_INFO ConsoleInfo;
+	GetConsoleScreenBufferInfo(console::output, &ConsoleInfo);
+	return ConsoleInfo.wAttributes;
+}
+
+void resettextattr()
+{
+	DWORD Mau_Mac_Dinh = textattr();
+	SetConsoleTextAttribute(console::output, Mau_Mac_Dinh);
+}
+
+void resettextattr()
+{
+	DWORD Mau_Mac_Dinh = textattr();
+	SetConsoleTextAttribute(console::output, Mau_Mac_Dinh);
+}
+
+void inChuCoMau(SHORT x, SHORT y, WORD color, WORD background, LPSTR str, ...)
+{
+	gotoXY(x, y);
+	datMauNen(background);
+	datMau(color);
+
+	/*In duoc nhieu chu hon*/
+	va_list args;
+	va_start(args, str);
+	vprintf(str, args);
+	va_end(args);
+	/*In duoc nhieu chu hon*/
+
+	resettextattr();
+	//datMau(7);
+}
+
+void gameMenu(short index) {
+	clrscr();
+	menu = 0;
+
+	LPSTR STRTextMenuChinh = const_cast<char*>(s.c_str());
+	inChuCoMau((ConsoleWidth / 2) - (strlen(STRTextMenuChinh) / 2), 2, 3, 0, STRTextMenuChinh);
+
+
+	currentIndex = index; //tuong tu tren
+	s = "1. Play";
+	STRTextMenuChinh = const_cast<char*>(s.c_str());
+	inChuCoMau((ConsoleWidth / 2) - (strlen(STRTextMenuChinh) / 2), 6, 15, ((currentIndex == 0) ? 2 : 0), STRTextMenuChinh);
+	s = "2. High score";
+	STRTextMenuChinh = const_cast<char*>(s.c_str());
+	inChuCoMau((ConsoleWidth / 2) - (strlen(STRTextMenuChinh) / 2), 7, 15, ((currentIndex == 1) ? 2 : 0), STRTextMenuChinh);
+	s = "3. Exit";
+	STRTextMenuChinh = const_cast<char*>(s.c_str());
+	inChuCoMau((ConsoleWidth / 2) - (strlen(STRTextMenuChinh) / 2), 8, 15, ((currentIndex == 2) ? 2 : 0), STRTextMenuChinh);
+
 }
--- menu.h
+++ menu.h
@@ -4,15 +4,21 @@ extern int menu, currentIndex;
 #define KEY_UP 0x26
 #define KEY_DOWN 0x28
 #define KEY_ESC 0x1B
+#define ConsoleWidth 1080
 void keyHandler(KEY_EVENT_RECORD key);
 void eventHandler();
 void clrscr(void);
 void gotoXY(SHORT x, SHORT y);
 SHORT wherex();
 SHORT wherey();
+void inChuCoMau(SHORT x, SHORT y, WORD color, WORD background, LPSTR str, ...);
+
 
 class console {
 public:
 	static HANDLE output;
 	static HANDLE input;
 };
+
+
+void gameMenu(short index);
