774f2070c86f4bb2d8f202b753681e520a5c9f12
Duc7111 <phantrungduc2003@gmail.com>
2022-11-26 23:40:31 +0700

    basic CGAME implementation

--- game.cpp
+++ game.cpp
@@ -1,4 +1,159 @@
 #include "game.h"
+
+enum gameState{END, CON, PAUSE};
+
+CGAME::CGAME() : level(0)
+{
+	//Add code if needed
+}
+
+CGAME::~CGAME()
+{
+	for (CVEHICLE* p : arrVehicle) delete p;
+	for (CANIMAL* p : arrAnimal) delete p;
+}
+
+CPEOPLE CGAME::getPeople()
+{
+	return cn;
+}
+
+vector<CVEHICLE*> CGAME::getVehicle()
+{
+	return arrVehicle;
+}
+
+vector<CANIMAL*> CGAME::getAnimal()
+{
+	return arrAnimal;
+}
+
+void CGAME::resetGame()
+{
+	level = 0;
+
+	for (CVEHICLE* p : arrVehicle) delete p;
+	arrVehicle.clear();
+
+	for (CANIMAL* p : arrAnimal) delete p;
+	arrAnimal.clear();
+}
+
+void CGAME::exitGame(HANDLE h) //later modify (depend on main)
+{
+
+}
+
+CVEHICLE* makeVehicle(bool b)
+{
+	if (b) return new CTRUCK;
+	else return new CCAR;
+}
+
+CANIMAL* makeAnimal(bool b)
+{
+	if (b) return new CDINAUSOR;
+	else return new CBIRD;
+}
+
+void CGAME::startGame()
+{
+	//Initialize
+	gameState gS;
+	char input;
+	//inputing thread
+
+	while (gS)
+	{
+		//data controlling
+		updatePosPeople(input);
+		updatePosAnimal();
+		updatePosVehicle();
+		//condition check
+		if (cn.isDead()) gS = END;
+
+		//screen drawing
+	}
+}
+
+void CGAME::loadGame(ifstream& fin)
+{
+	resetGame();
+	//level
+	fin.read((char*)&level, sizeof(level));
+	//Obtacles (vehicle & animal)
+	int n;
+	fin.read((char*)&n, sizeof(int));
+	arrVehicle.resize(n);
+	for (CVEHICLE* p : arrVehicle)
+	{
+		bool t;
+		fin.read((char*)&t, sizeof(bool));
+		p = makeVehicle(t);
+	}
+
+	fin.read((char*)&n, sizeof(int));
+	arrVehicle.resize(n);
+	for (CVEHICLE* p : arrVehicle)
+	{
+		bool t;
+		fin.read((char*)&t, sizeof(bool));
+		p = makeVehicle(t);
+	}
+	//People
+	fin.read((char*)&cn, sizeof(cn));
+}
+
+void CGAME::saveGame(ofstream& fout)
+{
+	//Level
+	fout.write((char*)&level, sizeof(level));
+	//Obstacle
+	int n = arrVehicle.size();
+	fout.write((char*)&n, sizeof(int));
+	for (CVEHICLE* p : arrVehicle)
+	{
+		bool b = typeid(*p) == typeid(CTRUCK);
+		fout.write((char*)&b, sizeof(b));
+		if(b) fout.write((char*)p, sizeof(CTRUCK));
+		else fout.write((char*)p, sizeof(CVEHICLE));
+	}
+	for (CANIMAL* p : arrAnimal)
+	{
+		bool b = typeid(*p) == typeid(CDINAUSOR);
+		fout.write((char*)&b, sizeof(b));
+		if (b) fout.write((char*)p, sizeof(CDINAUSOR));
+		else fout.write((char*)p, sizeof(CBIRD));
+	}
+	//People
+	fout.write((char*)&cn, sizeof(cn));
+}
+//Continnue when game is good designed
+void CGAME::pauseGame(HANDLE)
+{
+
+}
+
+void CGAME::resumeGame(HANDLE)
+{
+
+}
+
+void CGAME::updatePosPeople(char input)
+{
+
+}
+
+void CGAME::updatePosVehicle()
+{
+
+}
+
+void CGAME::updatePosAnimal()
+{
+
+}
+
 void playGame() {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "PROJECT CS202", sf::Style::Titlebar | sf::Style::Close);
 
--- game.h
+++ game.h
@@ -2,28 +2,35 @@
 #include "vehicle.h"
 #include "animal.h"
 #include "people.h"
+
 #include <Windows.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
 #pragma once
+
+#define LEVEL_MAX 5
+
 using namespace std;
-class CGAME {
-	CTRUCK* axt;
-	CCAR* axh;
-	CDINAUSOR* akl;
-	CBIRD* ac;
+class CGAME 
+{
+private:
+	unsigned short level;
+	vector<CVEHICLE*> arrVehicle;
+	vector<CANIMAL*> arrAnimal;
 	CPEOPLE cn;
 public:
 	CGAME(); //Chuẩn bị dữ liệu cho tất cả các đối tượng
 	void drawGame(); //Thực hiện vẽ trò chơi ra màn hình sau khi có dữ liệu
 	~CGAME(); // Hủy tài nguyên đã cấp phát
 	CPEOPLE getPeople();//Lấy thông tin người
-	CVEHICLE* getVehicle();//Lấy danh sách các xe
-	CANIMAL* getAnimal(); //Lấy danh sách các thú
+	vector<CVEHICLE*> getVehicle();//Lấy danh sách các xe
+	vector<CANIMAL*> getAnimal(); //Lấy danh sách các thú
 	void resetGame(); // Thực hiện thiết lập lại toàn bộ dữ liệu như lúc đầu
 	void exitGame(HANDLE); // Thực hiện thoát Thread
 	void startGame(); // Thực hiện bắt đầu vào trò chơi
-	void loadGame(istream); // Thực hiện tải lại trò chơi đã lưu
-	void saveGame(istream); // Thực hiện lưu lại dữ liệu trò chơi
+	void loadGame(ifstream&); // Thực hiện tải lại trò chơi đã lưu
+	void saveGame(ofstream&); // Thực hiện lưu lại dữ liệu trò chơi
 	void pauseGame(HANDLE); // Tạm dừng Thread
 	void resumeGame(HANDLE); //Quay lai Thread
 	void updatePosPeople(char); //Thực hiện điều khiển di chuyển của CPEOPLE
