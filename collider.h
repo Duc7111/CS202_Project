f430fc94baf56fff4c89f10a807c817d1cd8ac5f
trietng <93473943+trietng@users.noreply.github.com>
2022-12-03 10:02:15 +0700

    Revert "Cảm biến phát hiện xe xung quanh"
    This reverts commit 60d4b5016fd69df9ddb4d8a7cf109f150e15c81d.

--- collider.h
+++ collider.h
@@ -1,10 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include "vehicle.h"
 
 bool PixelPerfectCollision(const sf::Sprite& a, const sf::Sprite& b,
 	const sf::Image& imgA, const sf::Image& imgB);
 
-bool CarNearbySensor(CVEHICLE* vehicle, std::vector<CVEHICLE*> generatedVehicles);
+bool CarNearbySensor(const CVEHICLE*& vehicle, std::vector<CVEHICLE*> generatedVehicles);
 //nhan biet co xe xung quanh, dung trong den xanh den do
