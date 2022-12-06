1cd5196bff23d69166f24fd8810cd9b777455525
trietng <93473943+trietng@users.noreply.github.com>
2022-12-06 20:07:22 +0700

    New seed generation

--- seed.cpp
+++ seed.cpp
@@ -1,6 +1,6 @@
 #include "seed.h"
 
-SEED::SEED() : seed(10, base) {}
+SEED::SEED() : seed(7, base) {}
 
 SEED::SEED(const std::vector<SEED_T>& seed) : seed(seed) {}
 
@@ -10,4 +10,16 @@ SEED_T& SEED::operator[](size_t idx) {
 
 const SEED_T& SEED::operator[](size_t idx) const {
 	return seed[idx];
+}
+
+void SEED::generate() {
+	int numRoad = DICE::random(boundLower, boundUpper);
+	int road;
+	while (numRoad != 0) {
+		do {
+			road = DICE::random(0, 6);
+		} while (seed[road] != base);
+		seed[road] = DICE::flip() ? vehicle : animal;
+		--numRoad;
+	}
 }
--- seed.h
+++ seed.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <random>
 #include <vector>
+#include "dice.h"
 
 enum SEED_T : unsigned char {
 	base, vehicle, animal 
@@ -12,6 +12,9 @@ public:
 	SEED(const std::vector<SEED_T>& seed);
 	SEED_T& operator[](size_t idx);
 	const SEED_T& operator[](size_t idx) const;
+	void generate();
 private:
+	static const int boundLower = 3;
+	static const int boundUpper = 5;
 	std::vector<SEED_T> seed;
 };
