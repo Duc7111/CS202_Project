be745cbee56833a06e46513d29d6a9801f7e6102
viettungvuong <viettungvuong@gmail.com>
2022-12-11 20:11:02 +0700

    Thêm phần highscore

---
+++ highScore.cpp
@@ -0,0 +1,48 @@
+﻿#include <queue>
+#include <fstream>
+#include <string>
+#include "highScore.h"
+#include <iterator>
+
+std::multiset<int> highScore;
+
+void readHighScore() {
+	highScore.clear();
+
+	std::ifstream ifs;
+	ifs.open(fileName, std::ios::binary);
+	int size;
+	ifs.read((char*)&size, sizeof(int));
+	for (int i = 0; i < size; i++) {
+		int temp;
+		ifs.read((char*)&temp, sizeof(int));
+		highScore.insert(temp);
+	}
+
+	ifs.close();
+}
+
+void compareHighScore(int currentScore) //so sánh điểm hiện tại với top highscore
+{
+	std::multiset<int>::iterator it = highScore.end();
+	it--;
+	if (currentScore > *(it))
+	{
+		if (highScore.size() == 5)
+			highScore.erase(it);
+		highScore.insert(currentScore);
+		writeHighScore();
+	}
+}
+void writeHighScore() {
+	std::ofstream ofs;
+	ofs.open(fileName, std::ios::binary);
+	int size = highScore.size();
+	ofs.write((char*)&size, sizeof(int));
+	for (int score : highScore)
+		ofs.write((char*)&score, sizeof(int));
+	ofs.close();
+}
+void viewHighScore() {
+
+}
---
+++ highScore.h
@@ -0,0 +1,12 @@
+﻿#include <set>
+#include <fstream>
+#include <string>
+
+const std::string fileName = "highScore.dat";
+
+extern std::multiset<int> highScore;
+
+void readHighScore();
+void compareHighScore(int currentScore); //so sánh điểm hiện tại với top highscore
+void writeHighScore();
+void viewHighScore();
